2a1c2ffe9f03147bd25a413e50c4357be373a760
NerlyTon <nerlyton@gmail.com>
2020-09-01 21:29:48 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,9 +54,9 @@ class InteractiveRecord
     end
 
     def self.find_by(hash)
-        sql = "SELECT * FROM #{self.table_name} WHERE '#{hash.key[0].to_s}' = '#{hash.value[0].to_s}'"
+        sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
         DB[:conn].execute(sql)
-    end
+      end
 
 
   
